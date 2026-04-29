52d68132147e824138c9f8b8a1f7fd524c316419
lucasdbf_dev <lucasfernandes1829@gmail.com>
2026-04-28 21:27:59 -0300

    Add files via upload

---
+++ Aula 25/Aprovacao - Aula 25/src/Principal.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		String nome = "";
+		double mediaAluno = 7;
+		int frequenciaAluno = 75;
+		
+		
+		while(mediaAluno >= 7 && frequenciaAluno >= 75) {
+			System.out.println("Qual o nome do aluno?");
+			nome = input.nextLine();
+			
+			System.out.println("Qual a média do aluno?");
+			mediaAluno = input.nextDouble();
+			input.nextLine();
+			
+			System.out.println("Qual a frequência do aluno?");
+			frequenciaAluno = input.nextInt();
+			input.nextLine();
+			
+			if(mediaAluno >= 7 && frequenciaAluno >= 75) {
+				System.out.println("O aluno " + nome + " está aprovado!");	
+			}
+		}
+		
+		System.out.println("O aluno " + nome + " está reprovado!");
+		
+	}
+}
